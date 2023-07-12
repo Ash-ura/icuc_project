9fd96749095c61ec33e8f5a13feff258f126542d
Senam Philip Adagbe <95082111+Ash-ura@users.noreply.github.com>
2023-07-12 15:31:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ import secrets
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "mysql+pymysql://root:roottoor@localhost:3306/church"
+] = "mysql+pymysql://gxxv4os024sqljpnou7a:pscale_pw_S5gSv9BbDQJLOaUg8VCzmCzC5rumzO6jANtlmxMh4Ry@localhost:3306/church"
 app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
 db = SQLAlchemy(app)
