5b0505a740e5bdfa105be5121b6347a93de9d0de
Senam Philip Adagbe <95082111+Ash-ura@users.noreply.github.com>
2023-07-12 17:21:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ import secrets
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "mysql+pymysql://gxxv4os024sqljpnou7a:pscale_pw_S5gSv9BbDQJLOaUg8VCzmCzC5rumzO6jANtlmxMh4Ry@aws.connect.psdb.cloud/icuc_database"
+] = "mysql+pymysql://tvaqgz6f9vzaqu48y24b:pscale_pw_4nbBofhTkL4fc8S98NQTEWZU2hRhp0I7kNIeQ7SAKcF@aws.connect.psdb.cloud/icuc_database"
 app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
 db = SQLAlchemy(app)
