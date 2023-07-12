6e83ffa2e9b1047f8d3c4571f303ce5c149b9b4e
Senam Philip Adagbe <95082111+Ash-ura@users.noreply.github.com>
2023-07-12 18:04:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ import secrets
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://senam:qWCk6zCqVZlnvAXCu_Z1RQ@river-catfish-9550.6wr.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+] = "cockroachdb://senam:qWCk6zCqVZlnvAXCu_Z1RQ@river-catfish-9550.6wr.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
 app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
 db = SQLAlchemy(app)
