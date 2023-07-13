e789c891afa2d484210ad6aa81dcf00f78a54db5
Senam Philip Adagbe <95082111+Ash-ura@users.noreply.github.com>
2023-07-13 09:40:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,3 @@ app.config[
 ] = "cockroachdb://senam:qWCk6zCqVZlnvAXCu_Z1RQ@river-catfish-9550.6wr.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
 app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
 db = SQLAlchemy(app)
-db.create_all()
