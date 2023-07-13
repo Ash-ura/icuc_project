79f533ef49deedd3708d3e45840db752f514c621
Senam Philip Adagbe <95082111+Ash-ura@users.noreply.github.com>
2023-07-13 10:46:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ import subprocess
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "cockroachdb://senam:7qafKz1seSNjX0QHKINrOA@kind-ogre-8986.8nj.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+] = "cockroachdb://senam:7qafKz1seSNjX0QHKINrOA@kind-ogre-8986.8nj.cockroachlabs.cloud:26257/_migration_internal_church_sql_1_1689243872?sslmode=verify-full"
 app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
 db = SQLAlchemy(app)
