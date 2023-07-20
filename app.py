63eac4a0b86b126ab896cb00c9123909f826bd8b
david oppong-nkrumah <doppong-nkrumah@itconsortiumgh.com>
2023-07-20 08:42:11 +0000

    added ssl root certificate

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import secrets
-import subprocess
-
 
 
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "cockroachdb://senam:7qafKz1seSNjX0QHKINrOA@kind-ogre-8986.8nj.cockroachlabs.cloud:26257/icuc_database?sslmode=verify-full"
+] = "cockroachdb://senam:7qafKz1seSNjX0QHKINrOA@kind-ogre-8986.8nj.cockroachlabs.cloud:26257/icuc_database?" \
+    "sslmode=verify-full&sslrootcert=./root.crt"
 app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
 db = SQLAlchemy(app)
